aa6485338102d837a116923547c07cc85823ea34
gtortajada <gtortajada1@gmail.com>
2024-11-11 20:22:14 -0300

    Refatoração: remoção de comentário + adição da propriedade optimizePackageImports na configuração do next.js.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-{/* Não fiz a parte do tutorial do ChakraUi que altera este documento. O conteúdo que deveria estar aqui é: export default {
+const nextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
-} */}
+};
+
+export default nextConfig;
